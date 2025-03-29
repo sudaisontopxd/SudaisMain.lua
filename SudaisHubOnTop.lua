ae42aee86597c853120fb603b24d451bacb6b1a9
sudaisontopxd <xynxcheats@gmail.com>
2025-03-29 20:49:04 +0500

    SudaisHubOnTop.lua

---
+++ SudaisHubOnTop.lua
@@ -0,0 +1,13 @@
+if game.PlaceId == 13772394625 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sudaisontopxd/BladeBallScriptMadeBySudaisXCheatsMain/refs/heads/main/BladeBallOpScriptbysudais"))()
+elseif game.PlaceId == 5293755937 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sudaisontopxd/SpeedRunSimulatorByCheats/refs/heads/main/SpeedRumSimulator"))()
+elseif game.PlaceId == 3214114884 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/sudaisontopxd/FlagWarsScript/refs/heads/main/madeBySudais'))()
+elseif game.PlaceId == 13772394625 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sudaisontopxd/BladeBallScriptMadeBySudaisXCheatsMain/refs/heads/main/BladeBallOpScriptbysudais"))()
+elseif game.PlaceId == 17262329365 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sudaisontopxd/X500-CLICKS-Mega-Clickers/refs/heads/main/MegaCLickersSXC"))()
+else
+    game:GetService("Players").LocalPlayer:Kick("Get In A Script Supported Game")
+end
