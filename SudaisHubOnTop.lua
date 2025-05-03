97682e4a76950fd98af7a9df9a0f1a46f5be4df8
sudaisontopxd <xynxcheats@gmail.com>
2025-05-03 20:03:50 +0500

    SudaisHubOnTop.lua

--- SudaisHubOnTop.lua
+++ SudaisHubOnTop.lua
@@ -8,6 +8,8 @@ elseif game.PlaceId == 13772394625 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sudaisontopxd/BladeBallScriptMadeBySudaisXCheatsMain/refs/heads/main/BladeBallOpScriptbysudais"))()
 elseif game.PlaceId == 17262329365 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sudaisontopxd/X500-CLICKS-Mega-Clickers/refs/heads/main/MegaCLickersSXC"))()
+elseif game.PlaceId == 14330243992 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/sudaisontopxd/PowerSlapSim/refs/heads/main/pss"))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/sudaisontopxd/AllGamesHub/refs/heads/main/SudaisHubUniversalAllGames"))()
 end
